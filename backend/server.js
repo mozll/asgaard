93c6ca37c1726e9a8e2a689033273f4aa3e6ce3c
mozll <andreas_oh@outlook.com>
2024-04-30 23:32:42 +0200

    Opsætning af basic logik, react ts, express, db med xampp

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const mysql = require("mysql")
+const cors = require("cors")
+
+const app = express()
+app.use(cors())
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "test"
+
+})
+
+app.get("/", (req, res) => {
+    return res.json("from backend")
+})
+
+app.get("/users", (req, res)=> {
+    const sql = "SELECT * FROM users"
+    db.query(sql, (err, data)=> {
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+app.listen(8081, () => {
+    console.log("listening")
+})
---
+++ frontend/src/App.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react"
+
+interface Test {
+  user_name: string
+  user_id: number
+  user_email: string
+}
+
+function App() {
+  const [data, setData] = useState<Test[]>([])
+
+  useEffect(()=> {
+    fetch("http://localhost:8081/users") // if i actually search for this in the URL i also get the data shown
+    .then(res => res.json())
+    .then(data => setData(data))
+    .catch(err => console.log(err));
+  }, [])
+
+  return (
+    <div>
+ <table>
+  <thead>
+    <th>ID</th>
+    <th>Name</th>
+    <th>Email</th>
+  </thead>
+  <tbody>
+    {data.map((data, index) => (
+      <tr key={index}>
+        <td>{data.user_id}</td>
+<td>{data.user_name}</td>
+<td>{data.user_email}</td>
+      </tr>
+    ))}
+  </tbody>
+ </table>
+
+    </div>
+  )
+}
+
+export default App
