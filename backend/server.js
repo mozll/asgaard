6566383f5a780648ab37dfd579d36dd3764f0c35
mozll <andreas_oh@outlook.com>
2024-05-31 15:55:02 +0200

    removed unsuccessful attempt at fixing memory leak in backend

--- backend/server.js
+++ backend/server.js
@@ -66,7 +66,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // express session
 app.use(
   session({
-    store: new RedisStore({ client: redisClient }), // attempt to fix backend memory crash
     name: "nameOfCookieThatStoresSessionID",
     secret: "longRandomHash",
     resave: false,
